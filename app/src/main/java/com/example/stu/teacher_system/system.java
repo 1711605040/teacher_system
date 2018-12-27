14165e7dcea2dca80ad7b2f1655f3fbb0fda4037
Diana997 <15218650918@163.com>
2018-12-27 10:10:16 +0800

    声明控件

--- app/src/main/java/com/example/stu/teacher_system/system.java
+++ app/src/main/java/com/example/stu/teacher_system/system.java
@@ -2,9 +2,10 @@ package com.example.stu.teacher_system;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class system extends AppCompatActivity {
-
+EditText edtName,edtSex,edtNumber,edtTie,edtClass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

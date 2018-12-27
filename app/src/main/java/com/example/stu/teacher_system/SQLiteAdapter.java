bcda937cf384b2d32f732f1a5b970114eabeef84
1711605040 <nokia5424003@163.com>
2018-12-27 15:12:47 +0800

    功能实现

--- app/src/main/java/com/example/stu/teacher_system/SQLiteAdapter.java
+++ app/src/main/java/com/example/stu/teacher_system/SQLiteAdapter.java
@@ -1,7 +1,10 @@
 package com.example.stu.teacher_system;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.widget.Toast;
+import android.util.Log;
 
 public class SQLiteAdapter {
     SQLiteDatabase db=null;
@@ -10,9 +13,44 @@ public class SQLiteAdapter {
     public SQLiteAdapter(Context context) {
         this.context = context;
     }
-    public  void  Opendb(){
+    public  void  opendb(){
         Helper helper=new Helper(context,"teacher",null,1);
         db=helper.getWritableDatabase();
     }
+    public void closedb(){
+        if (db.isOpen()){
+            db.close();
+        }
+        db=null;
+    }
+    public boolean insert(Infor infor){
+        boolean result = false;
+        opendb();
+        ContentValues values = new ContentValues();
+        values.put("name",infor.getName());
+        values.put("phone",infor.getPhone());
+        values.put("sex",infor.getSex());
+        values.put("tie",infor.getTie());
+        values.put("class",infor.getBanji());
 
+        Long rowid = db.insert("information",null,values);
+        if(rowid!=-1){
+            result=true;
+            Toast.makeText(context,"添加成功",Toast.LENGTH_LONG).show();
+            Log.i("数据库操作","添加成功");
+       }else {
+           Toast.makeText(context,"添加失败",Toast.LENGTH_LONG).show();
+            Log.i("数据库操作失败","添加失败");
+       }
+
+        closedb();
+        return false;
+    }
+    public int deleteByName(String name){
+        int result = -1;
+        opendb();
+        result = db.delete("information", "name=?",new String[]{name});
+        closedb();
+        return result;
+    }
 }
--- app/src/main/java/com/example/stu/teacher_system/system.java
+++ app/src/main/java/com/example/stu/teacher_system/system.java
@@ -2,14 +2,17 @@ package com.example.stu.teacher_system;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class system extends AppCompatActivity {
 EditText edtName,edtSex,edtNumber,edtTie,edtClass;
 Button btnAdd,btnDisplay,btnDelete,btnModify;
 ListView lvShow;
+Infor infor=new Infor();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +30,32 @@ ListView lvShow;
 
         lvShow=findViewById(R.id.lv_show);
 
-
+     btnAdd.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+             String name=edtName.getText().toString().trim();
+             String sex=edtSex.getText().toString().trim();
+             String tie=edtTie.getText().toString().trim();
+             String banji=edtClass.getText().toString().trim();
+             Long number=Long.valueOf(edtNumber.getText().toString().trim());
+            infor.setName(name);
+            infor.setSex(sex);
+            infor.setBanji(banji);
+            infor.setTie(tie);
+            infor.setPhone(number);
+            SQLiteAdapter SQLiteadapter=new SQLiteAdapter(getApplicationContext());
+            SQLiteadapter.insert(infor);
+         }
+     });
+     btnDelete.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+             String name = edtName.getText().toString().trim();
+             SQLiteAdapter SQLiteadapter = new SQLiteAdapter(getApplicationContext());
+             int num = SQLiteadapter.deleteByName(name);
+             Toast.makeText(getApplicationContext(),"删除记录条数为"+String.valueOf(num),Toast.LENGTH_SHORT).show();
+         }
+     });
 
     }
 }

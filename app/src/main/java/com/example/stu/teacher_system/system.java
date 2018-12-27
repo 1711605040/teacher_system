3cab2c43bfee2176ccdd3a4e3849fa50abeb83f5
Diana997 <15218650918@163.com>
2018-12-27 14:27:50 +0800

    更新声明控件

--- app/src/main/java/com/example/stu/teacher_system/system.java
+++ app/src/main/java/com/example/stu/teacher_system/system.java
@@ -2,13 +2,32 @@ package com.example.stu.teacher_system;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 
 public class system extends AppCompatActivity {
 EditText edtName,edtSex,edtNumber,edtTie,edtClass;
+Button btnAdd,btnDisplay,btnDelete,btnModify;
+ListView lvShow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_system);
+        edtName=findViewById(R.id.edt_name);
+        edtSex=findViewById(R.id.edt_sex);
+        edtTie=findViewById(R.id.edt_tie);
+        edtClass=findViewById(R.id.edt_class);
+        edtNumber=findViewById(R.id.edt_number);
+
+        btnAdd=findViewById(R.id.btn_add);
+        btnDisplay=findViewById(R.id.btn_display);
+        btnDelete=findViewById(R.id.btn_delete);
+        btnModify=findViewById(R.id.btn_modify );
+
+        lvShow=findViewById(R.id.lv_show);
+
+
+
     }
 }
